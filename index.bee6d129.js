c5657803b4ff6584b89d04e39c1f65050d4bbc5e
David Moskalenko <dvdmsk21@gmail.com>
2025-02-16 00:04:55 +0200

    make build

---
+++ index.bee6d129.js
@@ -0,0 +1,2 @@
+var r,e,n,o=document.querySelector("table");r=Array.from(o.rows).map(function(r){return r.cells[1]}),e=Array.from(o.rows).map(function(r){return r.cells[4]}),n=r.map(function(r){return r.cloneNode(!0)}),e.forEach(function(r,e){return r.before(n[e])});
+//# sourceMappingURL=index.bee6d129.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,12 @@
+'use strict';
+// write your code here
+const table = document.querySelector('table');
+function cloneColum(tbl, col, targetCol) {
+    const columnsForCloning = Array.from(tbl.rows).map((x)=>x.cells[col]);
+    const targetColumns = Array.from(tbl.rows).map((x)=>x.cells[targetCol]);
+    const clonedCols = columnsForCloning.map((x)=>x.cloneNode(true));
+    targetColumns.forEach((x, i)=>x.before(clonedCols[i]));
+}
+cloneColum(table, 1, 4);
+
+//# sourceMappingURL=index.f75de5e1.js.map
